f1089f10fecac0145a59ecfc760f7db85daeef7c
Alexey Semikozov <forworknoname@gmail.com>
2017-06-19 11:48:08 +0300

    Refactor testcafe_runner

--- testcafe_runner.js
+++ testcafe_runner.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
+const yargs = require('yargs');
 const createTestCafe = require('testcafe');
 const { getInstallations } = require('testcafe-browser-tools');
 const fs = require('fs');
 let testcafe = null;
 process.env.DB = './__test_db__.json';
+
+const argv = yargs.option('browser', {
+    alias: 'b',
+    default: false
+}).help().argv
 
 getInstallations()
     .then((browsers) => {
@@ -11,15 +17,16 @@ getInstallations()
             .then((tc) => {
                 testcafe = tc;
                 const runner = testcafe.createRunner();
-                const browser = Object.keys(browsers).slice(0,1);
+                const browser = argv.browser || Object.keys(browsers).slice(0,1);
                 return runner
                     .startApp('npm run dev', 5000)
                     .src(['./tests/e2e-tests/index.test.js'])
                     .browsers(browser)
                     .run();
             })
-            .then(() => {
+            .then((exitCode) => {
                 testcafe.close();
                 fs.unlink(process.env.DB);
+                process.exit(exitCode)
             })
     });
